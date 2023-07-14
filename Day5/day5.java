74d59fe305789bd2ab991db013f297f77f01f599
Yuvan prasath <136918195+yuvanprasath24@users.noreply.github.com>
2023-07-14 22:24:47 +0530

    day5.java

---
+++ Day5/day5.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		for (int i=1;i<=t ;i++ ){
+		    int x=s.nextInt();
+		    int y=s.nextInt();
+		    if(x%y!=0){
+		        int a=x/y;
+		        int b=x%y;
+		        System.out.println(a+b);
+		    }else{
+		        System.out.println(x/y);
+		    }
+		} 
+	}
+}
