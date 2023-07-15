25d41f99028e0ae317496af5da0de5da5f2bd529
Yuvan prasath <136918195+yuvanprasath24@users.noreply.github.com>
2023-07-15 22:37:17 +0530

    day6.java

---
+++ Day6/day6.java
@@ -0,0 +1,29 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		for(int i=1;i<=t;i++){
+		    int n=s.nextInt();
+		    int m=s.nextInt();
+		    double o=n-n*0.1;
+		    if(o<m){
+		        System.out.println("ONLINE");
+		    }else if(o>m){
+		        System.out.println("DINING");
+		    }else if(o==m){
+		        System.out.println("EITHER");
+		    }
+		    
+		}
+	}
+}
