11c93c0b53c66deaae0291128dcd48cbb81a3784
Yuvan prasath <136918195+yuvanprasath24@users.noreply.github.com>
2023-07-19 22:17:51 +0530

    day10.java

---
+++ Day10/day10.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		while(t --> 0){
+		    int h=s.nextInt();
+		    int x=s.nextInt();
+		    int y=s.nextInt();
+		    int c=s.nextInt();
+		    
+		    int z=x+y/2;
+		    int a=h*z;
+		    if (a<=c)
+		    System.out.println("YES");
+		    else
+		    System.out.println("NO");
+		    
+		}
+	}
+}
