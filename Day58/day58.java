02296ee9810fef2874925b1f592e02bffdb47752
Yuvan prasath <136918195+yuvanprasath24@users.noreply.github.com>
2023-09-12 22:12:05 +0530

    day58.java

---
+++ Day58/day58.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index = 1;
+        int k = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            int first = nums[i];
+            int second = nums[i + 1];
+            if (first != second) {
+                nums[index] = second; 
+                index++;
+                k++;
+            }
+        }
+        return k;
+    }
+}
