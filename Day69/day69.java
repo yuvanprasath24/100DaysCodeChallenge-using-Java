fbd80462520766fc2d6a921273ac3e5dfd557784
Yuvan prasath <136918195+yuvanprasath24@users.noreply.github.com>
2023-09-23 23:27:29 +0530

    day69.java

---
+++ Day69/day69.java
@@ -0,0 +1,35 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		while(t-->0){
+		    int aple=s.nextInt();
+		    int ornge=s.nextInt();
+		    
+		    if(aple==1 || ornge==1){
+		        System.out.println("1");
+		    }
+		    else{
+		        while(aple!=ornge){
+		            if(aple>ornge){
+		                aple-=ornge;
+		            }
+		            else if(ornge>aple){
+		                ornge-=aple;
+		            }
+		        }
+		        System.out.println(aple);
+		}
+	}
+}
+}
