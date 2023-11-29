3a041e1f726f02db8b79a7118c4c151411b546ad
Yuvan prasath <136918195+yuvanprasath24@users.noreply.github.com>
2023-11-29 21:00:49 +0530

    day95.java

---
+++ Day95/day95.java
@@ -0,0 +1,31 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		while(t-->0){
+		    int N=s.nextInt();
+		    int A=s.nextInt();
+		    int B=s.nextInt();
+		    String dis=s.next();
+		    int district=0,state=0;
+		    for(int i=0;i<N;i++){
+		        if(dis.charAt(i)=='0')
+		            district++;
+		        else
+		            state++;
+		    }
+		    int time=(district*A)+(state*B);
+		    System.out.println(time);
+		}
+	}
+}
